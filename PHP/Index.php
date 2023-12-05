1c7a200cc09af50293f6b91c42525848bd71ec64
MannuGA <j.mannuelg.a@gmail.com>
2023-12-04 18:44:46 -0600

    Inicio

---
+++ PHP/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ea">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chamba Searcht</title>
+</head>
+<body>
+    <?php
+        include '../PHP/header.php'
+    ?>
+    
+</body>
+</html> 
---
+++ PHP/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/header.css">
+</head>
+<body>
+    <header>
+        <h1>Chamba x Search</h1>
+    </header>
+
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Acerca de</a>
+        <a href="#">Servicios</a>
+        <a href="#">Contacto</a>
+    </nav>
+
+    <!-- Contenido de la página va aquí -->
+
+</body>
+</html>
