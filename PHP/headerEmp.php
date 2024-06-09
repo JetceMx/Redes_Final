4de561e6cf3514a3a1e9453877c02228a294f1a9
Jacqueline-Renito <al291218@edu.uaa.mx>
2024-06-08 19:43:37 -0600

    Arreglo rutas absolutas

--- PHP/headerEmp.php
+++ PHP/headerEmp.php
@@ -12,7 +12,7 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand" href="../PHP/Index.php">
-                <img src="/IMG/logo.png" alt="Logo" height="80" class="d-inline-block align-text-top">
+                <img src="../IMG/logo.png" alt="Logo" height="80" class="d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
